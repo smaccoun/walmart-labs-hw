85e27a7b101c8700350d71253b85cddb9e1a23f3
Steven MacCoun <theraccoun@gmail.com>
2018-06-14 20:29:40 -0600

    Cleaner query param on fetch

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,23 +1,64 @@
 import {defaultGetRequestHttp, remoteRequest} from "./request";
+import {notAsked} from "./remote-data";
+const queryString = require('query-string');
 
 const BASE_URL = new URL('https://api.walmartlabs.com/v1/')
-const API_KEY = 'apiKey=dbdwpck6aeu2yst592ee4buq'
+const API_KEY = 'dbdwpck6aeu2yst592ee4buq'
 
-export function getUrl(endpoint: string): URL {
+const API_KEY_PARAM = {apiKey: API_KEY}
+
+interface IFetchInfo {
+    endpoint: string
+    params: null | Object
+}
+
+const productUrl = (producId: number): IFetchInfo => {
+    return {
+        endpoint: `items/${producId.toString()}`,
+        params: null
+    }
+}
+
+const searchUrl = (query: string) => {
+    return {
+        endpoint: 'search',
+        params:  {query}
+
+    }
+}
+
+const recommendedUrl = (itemId: number) => {
+    return {
+        endpoint: 'nbp',
+        params:  {itemId}
+
+    }
+}
+
+
+
+export function getUrl(fetchInfo: IFetchInfo): URL {
+    const authedParams = Object.assign(API_KEY_PARAM, fetchInfo.params)
+    const params  = queryString.stringify(authedParams)
+    const endpoint = `${fetchInfo.endpoint}?${params}`
     return new URL(endpoint, BASE_URL)
 }
 
 export function fetchItem(itemId: number){
-    const url = getUrl('items/'+ itemId.toString() + '?' + API_KEY)
+    const url = getUrl(productUrl(itemId))
     return remoteRequest(url.toString(), defaultGetRequestHttp)
 }
 
 export function fetchSearch(term: string | null){
-    const url = getUrl('search?'+ ('query=' + term)  + '&format=json' + ('&' + API_KEY))
-    return remoteRequest(url.toString(), defaultGetRequestHttp)
+    if(term == null){
+        return notAsked
+    }else{
+        const url = getUrl(searchUrl(term))
+        return remoteRequest(url.toString(), defaultGetRequestHttp)
+    }
 }
 
 export function fetchRecommended(productId: number){
-    const url = getUrl('nbp?' + API_KEY + ('&itemId=' + productId))
+    const url = getUrl(recommendedUrl(productId))
     return remoteRequest(url.toString(), defaultGetRequestHttp)
 }
